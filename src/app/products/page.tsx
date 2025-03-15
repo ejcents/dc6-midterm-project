709ab11d30744740f7484e3bfb9c7e895ed84757
ejcents <mdc.ejcentillas@gmail.com>
2025-03-15 14:37:58 +0800

    changes

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -19,7 +19,7 @@ export default async function ProductsPage() {
 
   try {
     products = await fetchProducts();
-  // para mawala ang error issue nga wa ma define ang "error" sa catch (quick fix sa vscode)
+  // para mawala ang error issue nga wa ma define ang "error" sa try-catch (quick fix sa vscode)
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return <p className="text-center capitalize text-red-500">failed to load products.</p>;
@@ -27,7 +27,7 @@ export default async function ProductsPage() {
 
   return (
     <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-3xl font-bold capitalize text-center mb-6">products</h1>
+      <h1 className="text-4xl font-extrabold capitalize text-center mb-6">products</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
